cfc0d79cc1adc5e529dfb3f4c2d45f35d95ec836
Travis Grigsby <travis.grigsby@gmail.com>
2013-09-23 18:31:53 -0700

    Initialize Mothafucking Repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'b6df09df872f42a6df7604e318535fdf096452a385da76280579c60415b337265b56fd14d9f58677f228c022be46411845430ecca9c1264e526fb1c92d56ba49'
